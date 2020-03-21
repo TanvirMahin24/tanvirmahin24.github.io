98e44c9e4d901fede1246ecde34b36d8d577c5fd
TanvirMahin24 <55342128+TanvirMahin24@users.noreply.github.com>
2020-03-21 13:56:37 +0600

    contact form back end

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*
+const form = document.querySelector('#contact-form');
+const sub = document.querySelector('#subject');
+const body = document.querySelector('#body');
+let url = 'mailto:tanvirmahin24@gmail.com';
+
+let subText ='';
+let bodyText = '';
+
+
+
+    
+
+
+
+form.addEventListener('submit', (e) => {
+    subText = encodeURIComponent(sub.value); 
+    bodyText = encodeURIComponent(body.value);
+    url += (`?subject=${subText}&body=${bodyText}`);
+    
+    form.action = url;
+    
+    console.log(form);
+    //e.preventDefault();
+});
+
+*/
